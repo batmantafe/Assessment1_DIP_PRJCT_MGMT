517d66872b4b80705698a3503328ca7dfc3fb918
Duane Reymond <duane.reymond@gmail.com>
2018-02-16 18:26:49 +1100

    Maze Light interaction with Enemy started.

---
+++ Assets/Scripts/Lights/ShiningLight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShiningLight : MonoBehaviour
+
+{
+    public Renderer colourRend;
+
+    public Light shiningLight;
+
+    // Use this for initialization
+    void Start()
+    {
+        shiningLight = GetComponent<Light>();
+        colourRend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            colourRend.material.color = Color.black;
+            shiningLight.enabled = false;
+        }
+    }
+}
