248710cec82922a23f9ccdb370a30b0bdd3344c2
Duane Reymond <duane.reymond@gmail.com>
2018-02-17 09:05:24 +1100

    Exit Door locations placed. EnemyManager gameobject and AIRandomStart scipt now renamed to GameManager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemyArray;
+    public GameObject[] exitDoorArray;
+
+    private int enemyStart;
+    private int exitDoorStart;
+
+    // Use this for initialization
+    void Start()
+    {
+        EnemyStartFunction();
+        ExitDoorFunction();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void EnemyStartFunction()
+    {
+        enemyStart = Random.Range(1,4);
+
+        enemyArray[enemyStart].SetActive(true);
+    }
+
+    void ExitDoorFunction()
+    {
+        exitDoorStart = Random.Range(1,4);
+
+        exitDoorArray[exitDoorStart].SetActive(true);
+    }
+}
